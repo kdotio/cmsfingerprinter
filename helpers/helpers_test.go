a48283fd8aebb2a48c5a24d704c92e8cc98f4017
kdotio <111248202+kdotio@users.noreply.github.com>
2022-08-22 19:20:04 +0200

    test(helpers): add sorting testcase

--- helpers/helpers_test.go
+++ helpers/helpers_test.go
@@ -1,22 +1,35 @@
 package helpers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSortRevs(t *testing.T) {
-	unsorted := []string{"1.7.9", "1.7.8", "1.7.7", "1.7.6", "1.7.5", "1.7.4", "1.7.3", "1.7.24", "1.7.2", "1.7.12", "1.7.10", "1.7.1", "1.7.0"}
-	t.Log("Input:", unsorted)
-
-	want := []string{"1.7.24", "1.7.12", "1.7.10", "1.7.9", "1.7.8", "1.7.7", "1.7.6", "1.7.5", "1.7.4", "1.7.3", "1.7.2", "1.7.1", "1.7.0"}
-
-	got := SortRevsAlphabeticallyDesc(unsorted)
-	if len(unsorted) != len(got) {
-		t.Fatal("length differs")
+	cases := []struct {
+		input []string
+		want  []string
+	}{
+		{[]string{"1.7.9", "1.7.8", "1.7.7", "1.7.6", "1.7.5", "1.7.4", "1.7.3", "1.7.24", "1.7.2", "1.7.12", "1.7.10", "1.7.1", "1.7.0"},
+			[]string{"1.7.24", "1.7.12", "1.7.10", "1.7.9", "1.7.8", "1.7.7", "1.7.6", "1.7.5", "1.7.4", "1.7.3", "1.7.2", "1.7.1", "1.7.0"}},
+		{[]string{"5.6.0", "5.7", "5.7.1"}, []string{"5.7.1", "5.7", "5.6.0"}},
 	}
-	t.Log("Output:", got)
 
-	for i := range unsorted {
-		if want[i] != got[i] {
-			t.Errorf("order does not match (%d), expected '%s', got '%s'", i, want[i], got[i])
-		}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Log("Input:", tc.input)
+
+			got := SortRevsAlphabeticallyDesc(tc.input)
+			if len(tc.input) != len(got) {
+				t.Fatal("length differs")
+			}
+			t.Log("Output:", got)
+
+			for i := range tc.input {
+				if tc.want[i] != got[i] {
+					t.Errorf("order does not match (%d), expected '%s', got '%s'", i, tc.want[i], got[i])
+				}
+			}
+		})
 	}
 }
